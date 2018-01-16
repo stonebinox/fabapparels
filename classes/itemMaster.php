3329fed9da91154cb33791b2a562c20175c2b851
Anoop S <anoop.santhanam@gmail.com>
2018-01-16 21:32:25 +0530

    First push

--- classes/itemMaster.php
+++ classes/itemMaster.php
@@ -96,9 +96,74 @@ class itemMaster extends inventoryMaster
             return "INVALID_ITEM_ID";
         }
     }
-    function getItems()
+    function getItems($inventoryID=NULL,$offset=0)
     {
-        
+        $app=$this->app;
+        $offset=secure($offset);
+        if((validate($offset))&&(is_numeric($offset))&&($offset>=0))
+        {
+            $im="SELECT iditem_master FROM item_master WHERE stat='1'";
+            if(validate($inventoryID))
+            {
+                inventoryMaster::__construct($inventoryID);
+                if($this->inventoryValid)
+                {
+                    $im.=" AND inventory_master_idinventory_master='$inventoryID'";
+                }
+            }
+            $im.=" ORDER BY iditem_master ASC LIMIT $offset,100";
+            $im=$app['db']->fetchAll($im);
+            $itemArray=array();
+            foreach($im as $item)
+            {
+                $itemID=$item['iditem_master'];
+                $this->__construct($itemID);
+                $itemData=$this->getItem();
+                if(is_array($itemData))
+                {
+                    array_push($itemArray,$itemData);
+                }
+            }
+            if(empty($itemArray))
+            {
+                return "NO_ITEMS_FOUND";
+            }
+            return $itemArray;
+        }
+        else
+        {
+            return "INVALID_OFFSET_VALUE";
+        }
+    }
+    function searchItem($text)
+    {
+        $text=trim(secure($text));
+        if(validate($text))
+        {
+            $im="SELECT iditem_master FROM item_master WHERE stat='1' AND item_name='$text' ORDER BY item_name ASC";
+            $app=$this->app;
+            $im=$app['db']->fetchAssoc($im);
+            $itemArray=array();
+            foreach($im as $item)
+            {
+                $itemID=$item['iditem_master'];
+                $this->__construct($itemID);
+                $itemData=$this->getItem();
+                if(is_array($itemData))
+                {
+                    array_push($itemArray,$itemData);
+                }
+            }
+            if(empty($itemArray))
+            {
+                return "NO_ITEMS_FOUND";
+            }
+            return $itemArray;
+        }
+        else
+        {
+            return "INVALID_SEARCH_TEXT";
+        }
     }
 }
 ?>
--- web/index.php
+++ web/index.php
@@ -31,6 +31,5 @@ $app->before(function(Request $request) use($app){
 $app->get("/",function() use($app){
     $app['twig']->render("index.html.twig");
 });
-
 $app->run();
 ?>
