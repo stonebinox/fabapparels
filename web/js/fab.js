707adc862ada767fd05f28b7b2b9641c32ed9c15
Anoop S <anoop.santhanam@gmail.com>
2018-05-02 17:25:16 +0530

    adding items

--- web/js/fab.js
+++ web/js/fab.js
@@ -198,7 +198,7 @@ app.controller("dashboard",function($scope,$http,$compile){
             $http.get("api/getItems/"+$scope.inventory_id)
             .then(function success(response){
                 response=response.data;
-                console.log(response);
+                console.log(response,$scope.inventory_id);
                 if(typeof(response)=="object"){
                     $scope.itemArray=response;
                     $scope.displayItemData();
